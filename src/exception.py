8b6c445cecb29261d89778c9ccb4a237440c2817
Alan <alanunni3@gmail.com>
2025-03-13 13:45:53 +0530

    Logging and exception

---
+++ src/exception.py
@@ -0,0 +1,18 @@
+import sys
+
+def error_message_details(error,error_details:sys):
+    _,_,exc_tb = error_details.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_message = "Error in python script name [{1}], line number [{2}], error message [{3}]".format(file_name,line_number,str(error))
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_details:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message,error_details)
+        
+    def __str__(self):
+        return self.error_message
+    
