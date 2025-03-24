6b6045c84fb6bd16f00de57fce3d328d389ab63d
Alan <alanunni3@gmail.com>
2025-03-24 22:59:16 +0530

    Predictive pipeline added

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import dill
 import os
 import sys
 import pickle
+from sklearn.model_selection import GridSearchCV
 
 from sklearn.metrics import r2_score
 from src.exception import CustomException
@@ -21,14 +22,21 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
             
 
-def evaluate_model(X_train, y_train, X_test,y_test,models):
+def evaluate_model(X_train, y_train, X_test,y_test,models,param):
     try:
         
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
             
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+            
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
+            # model.fit(X_train,y_train)
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
             
@@ -42,3 +50,12 @@ def evaluate_model(X_train, y_train, X_test,y_test,models):
         logging.error("Failed to evaluate the model")
         raise CustomException(e,sys)
     
+def load_object(file_path):
+    try:
+        if not os.path.exists(file_path):
+            raise FileNotFoundError(f"Error: {file_path} not found!")
+        with open(file_path,"rb") as file:
+            return pickle.load(file)
+    except Exception as e:
+        logging.error(f"Failed to load the object from {file_path}")
+        raise CustomException(e,sys)
